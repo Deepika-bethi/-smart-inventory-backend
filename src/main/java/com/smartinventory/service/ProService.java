574359c55b722e084b02e78bb8bdead8543b334f
Deepika-bethi <23eg104d16@anurag.edu.in>
2026-03-25 22:43:26 +0530

    CORS + Pro API

---
+++ src/main/java/com/smartinventory/service/ProService.java
@@ -0,0 +1,23 @@
+package com.smartinventory.service;
+
+import com.example.inventory.entity.Pro;
+import com.example.inventory.repository.ProRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProService {
+
+    @Autowired
+    private ProRepository proRepository;
+
+    public List<Pro> findAll() {
+        return proRepository.findAll();
+    }
+
+    public Pro save(Pro pro) {
+        return proRepository.save(pro);
+    }
+}
